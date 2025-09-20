94362c1b8ad44f41514ceffb08b9e152dc4ea90a
pranav2031 <pranavsonawane562@gmail.com>
2025-09-20 18:10:10 +0530

    2nd commit

--- Terraform-Automation-main/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "mydev-project-terraform-sample-batch-aws-devops-azure-29"
-    key = "main"
-    region = "us-east-1"
-    dynamodb_table = "my-dynamodb-table"
-  }
-}
